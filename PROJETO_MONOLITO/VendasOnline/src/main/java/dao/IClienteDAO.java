3b3bfc961f174fe12e5efb02ab5df1d83fd07f3f
Thiago <tlucenalive@gmail.com>
2023-12-07 09:11:38 -0300

    Projeto Monolitico em Java

---
+++ PROJETO_MONOLITO/VendasOnline/src/main/java/dao/IClienteDAO.java
@@ -0,0 +1,18 @@
+package dao;
+
+import java.util.List;
+
+import dao.generic.IGenericDAO;
+import domain.Cliente;
+
+
+
+
+	
+	public interface IClienteDAO extends IGenericDAO<Cliente, Long>{
+
+		List<Cliente> filtrarClientes(String query);
+
+	}
+
+
---
+++ PROJETO_MONOLITO/VendasOnline/src/main/java/service/IClienteService.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package service;
+
+import java.util.List;
+
+
+import domain.Cliente;
+import exceptions.DAOException;
+import services.generic.IGenericService;
+
+/**
+ * @author rodrigo.pires
+ *
+ */
+public interface IClienteService extends IGenericService<Cliente, Long> {
+
+	Cliente buscarPorCPF(Long cpf) throws DAOException;
+
+	List<Cliente> filtrarClientes(String query);
+
+}
